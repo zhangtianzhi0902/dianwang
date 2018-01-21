8340bfce4335f2fd403213adc25a41f1c71baef7
vagrant <vagrant@homestead>
2018-01-21 05:43:22 +0000

    Finish layouts and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+      <a href="/">
+        技术支持：皮卡丘的十万伏特
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{route('about')}}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello Grid Fault Detection App</h1>
+    <p class="lead">
+      欢迎来到 <a href="/">电网故障检测 App</a>
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{'signup'}}" role="button">现在注册</a>
+    </p>
+  </div>
+@stop
